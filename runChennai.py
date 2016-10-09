38f3aeb6f5919ef11d021d48d3560688fa2a3a5b
Dave <davekennewell@gmail.com>
2016-10-09 22:09:42 +0400

    updated DEM to allow straighter boundaries around catchment

--- runChennai.py
+++ runChennai.py
@@ -29,11 +29,11 @@ def run_chennai(sim_id):
     print "outputs_dir = " + outputs_dir
 
     # get data
-    # print "downloading data..."
-    # urllib.urlretrieve(
-    #     'http://chennaifloodmanagement.org/uploaded/layers/_30sdem_utm44.tif',
-    #     inputs_dir + '30sdem_utm44.tif'
-    # )
+    print "downloading data..."
+    urllib.urlretrieve(
+        'http://chennaifloodmanagement.org/uploaded/layers/utm44_1arc_v3.tif',
+        inputs_dir + 'utm44_1arc_v3.tif'
+    )
 
     print os.listdir(inputs_dir)
 
@@ -50,56 +50,38 @@ def run_chennai(sim_id):
 
     print "# starting"
     bounding_polygon_01 = [
-            [308878.53905281, 1485904.92065102],
-            [334106.6163469, 1488821.46137866],
-            [345335.29814831, 1494217.06172479],
-            [352772.47700379, 1494217.06172479],
-            [353209.95811293, 1489113.11545142],
-            [357001.46105887, 1488967.28841504],
-            [361084.61807756, 1492612.96432459],
-            [369396.75915133, 1491008.86692439],
-            [370709.20247877, 1486050.7476874],
-            [375400.94732484, 1479432.94037165],
-            [381937.88428019, 1480509.32030489],
-            [380917.09502551, 1486925.70990569],
-            [388354.27388099, 1487946.49916037],
-            [390104.19831758, 1491154.69396077],
-            [418977.95152121, 1498300.21874349],
-            [422267.27713657, 1487748.88524648],
-            [420389.80144202, 1483092.16931405],
-            [426415.13037669, 1478759.3958683],
-            [427290.09259498, 1466947.40592136],
-            [423206.93557629, 1455572.89708357],
-            [420914.4386505, 1445638.74373844],
-            [420910.51650151, 1445621.74775951],
-            [420910.15975327, 1445620.2018505],
-            [417453.11085711, 1429235.89524098],
-            [408296.12110623, 1390825.69292997],
-            [400348.54762341, 1394690.10939409],
-            [398525.70966864, 1415980.85670586],
-            [381609.77344833, 1439240.26900878],
-            [336695.04624268, 1427282.45202546],
-            [322112.34260448, 1439240.26900879],
-            [315695.95300367, 1474530.41181322]
-        ]
-    boundary_tags_01 = {'inland': range(0, 15) + range(25, 31), 'ocean': range(15, 25)}
+        [303382.14647903712, 1488780.8996663219],
+        [351451.89152459265, 1499834.3704521982],
+        [378957.03975921532, 1493150.8764886451],
+        [422656.80798244767, 1504204.3472745214],
+        [433196.16384805075, 1471300.9923770288],
+        [421885.63560203766, 1413463.0638462803],
+        [408261.59021479468, 1372590.9276845511],
+        [371245.31595511554, 1427344.16669366],
+        [316492.0769460068, 1417833.0406686035],
+        [303382.14647903712, 1488780.8996663219]
+    ]
+    boundary_tags_01 = {
+        'inland': [0, 1, 2, 6, 7, 8],
+        'ocean': [3, 4, 5]
+    }
     print "# Create domain:"
     print "# mesh_filename = " + working_dir + 'mesh_01.msh'
     domain = anuga.create_domain_from_regions(bounding_polygon=bounding_polygon_01,
                                               boundary_tags=boundary_tags_01,
                                               mesh_filename=working_dir + 'mesh_01.msh',
-                                              maximum_triangle_area=30000,
+                                              maximum_triangle_area=100000,
                                               verbose=True)
     domain.set_name(sim_id)
     domain.set_datadir(outputs_dir)
     poly_fun_pairs = [
             [
                 'Extent',
-                inputs_dir + '30sdem_utm44.tif'
+                inputs_dir + 'utm44_1arc_v3.tif'
             ]
     ]
     print "# create topography_function"
-    print "input raster = " + inputs_dir + '30sdem_utm44.tif'
+    print "input raster = " + inputs_dir + 'utm44_1arc_v3.tif'
     topography_function = qs.composite_quantity_setting_function(
         poly_fun_pairs,
         domain,
@@ -108,8 +90,8 @@ def run_chennai(sim_id):
     print topography_function
     print "# set_quantity elevation"
     domain.set_quantity('elevation', topography_function)  # Use function for elevation
-    domain.set_quantity('friction', 0.1)  # Constant friction
-    domain.set_quantity('stage', 2)  # Constant initial stage
+    domain.set_quantity('friction', 0.03)  # Constant friction
+    domain.set_quantity('stage', 1)  # Constant initial stage
 
     print "# all quantities set"
 
@@ -143,7 +125,7 @@ def run_chennai(sim_id):
         print domain.timestepping_statistics()
 
     asc_out_momentum = outputs_dir + '/' + sim_id + '_momentum.asc'
-    asc_out_stage = outputs_dir + '/' + sim_id + '_stage.asc'
+    asc_out_depth = outputs_dir + '/' + sim_id + '_depth.asc'
 
     anuga.sww2dem(outputs_dir + '/' + sim_id + '.sww',
                   asc_out_momentum,
@@ -153,14 +135,14 @@ def run_chennai(sim_id):
                   reduction=max,
                   verbose=True)
     anuga.sww2dem(outputs_dir + '/' + sim_id + '.sww',
-                  asc_out_stage,
+                  asc_out_depth,
                   quantity='depth',
                   number_of_decimal_places=3,
                   cellsize=30,
                   reduction=max,
                   verbose=True)
 
-    outputs =[asc_out_stage, asc_out_momentum]
+    outputs =[asc_out_depth, asc_out_momentum]
 
     for output in outputs:
         print "# Convert ASCII grid to GeoTiff so geonode can import it"
